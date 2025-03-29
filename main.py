30090f8a534b0f9ca8d082419c536e3c69a39ef7
BlindProphet32 <nwagner3@gmail.com>
2025-03-29 05:28:39 -0400

    Added screen and player

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
@@ -11,14 +12,24 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        updateable.update(dt)
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 
 if __name__ == "__main__":
